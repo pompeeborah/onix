eaa43ab72b8eb186f8489e1e979cf0e9d4b3474a
OlfactoryNinja <mccann.graham@gmail.com>
2014-07-03 14:40:39 +0100

    Latest changes

--- www/scr/onix.js
+++ www/scr/onix.js
@@ -1,10 +1,23 @@
-function listTests()
+String.prototype.capitalise = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
+function getList()
 {
     $.getJSON("api.php?method=list", function(data) {
-        console.log(data);
+        if (data.Status == "Y") {
+            var suite_template = Handlebars.compile($("#suite-template").html());
+            var test_template = Handlebars.compile($("#test-template").html());
+            for (suite in data.Results) {
+                $('section.main').append(suite_template({suite_name: suite.capitalise()}));
+                for (test in data.Results[suite]) {
+                    $('section.main').append(test_template({test_name: test}));
+                }
+            }
+        }
     });
 }
 
 $(document).ready(function() {
-    listTests();
+    getList();
 });
